483309da1ac16718c5dced4736c639b88b41b5bc
rolandvaszi <rolandvaszi@gmail.com>
2019-10-24 16:33:00 +0300

    Database created.

--- app/src/main/java/com/example/pointpoker/MainActivity.java
+++ app/src/main/java/com/example/pointpoker/MainActivity.java
@@ -3,19 +3,29 @@ package com.example.pointpoker;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText loginText;
+    DatabaseHelper databaseHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginText = findViewById(R.id.nameText);
     }
 
     public void login(View view) {
-        final Intent intent = new Intent(this, VoteActivity.class);
+        Intent intent = new Intent(this, VoteActivity.class);
         this.startActivity(intent);
+    }
+
+    private void toastMessage(String message){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
